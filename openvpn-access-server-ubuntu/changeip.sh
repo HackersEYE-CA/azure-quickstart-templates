950e26f8ae54270612941c246c6a1872f25d911d
HackersEYE-CA <67216442+HackersEYE-CA@users.noreply.github.com>
2020-06-21 21:43:11 +0300

    Update changeip.sh

--- openvpn-access-server-ubuntu/changeip.sh
+++ openvpn-access-server-ubuntu/changeip.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-PUBLICIP=$(curl -s ifconfig.me) && sudo sqlite3 "/usr/local/openvpn_as/etc/db/config_local.db" "update config set value='$PUBLICIP' where name='host.name';" && sudo systemctl restart openvpnas
+PUBLICIP=$(curl -s ifconfig.me)
+sudo sqlite3 /usr/local/openvpn_as/etc/db/config_local.db "update config set value='$PUBLICIP' where name='host.name';"
+sudo systemctl restart openvpnas
+
